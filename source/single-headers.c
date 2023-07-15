5308ce5864fcdd23755e220f291a50845831da48
Colleague Riley <ColleagueRiley@gmail.com>
2023-07-14 22:35:55 -0400

    updated headers

--- source/single-headers.c
+++ source/single-headers.c
@@ -16,14 +16,16 @@
 #define RLGL_IMPLEMENTATION
 #define RGFW_IMPLEMENTATION 
 #define RGFW_NO_JOYSTICK_CODES
-#define SI_IMPLEMENTATION 
+#define SI_IMPLEMENTATION
 
 #define GLFONTSTASH_IMPLEMENTATION
-#define FONTSTASH_IMPLEMENTATION
 #define STB_TRUETYPE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #define SI_ALLOCATOR_UNDEFINE
 
-
+#include "stb_image.h"
+#include "stb_image_write.h"
 #include "rlgl.h" /* lightweight OpenGL abstaction layer */
 #include "RGFW.h" /* lightweight GUI framework library */
 #include "rglfontstash.h" /* font rendering library on top of stb_truetype */
